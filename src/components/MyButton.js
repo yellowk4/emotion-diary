e4ea48f5da5474808a262fe0908eed77371d3589
younghun1982 <younghun1982@gmail.com>
2023-12-03 22:41:18 +0900

    집에서 저장

---
+++ src/components/MyButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MyButton = ({ text, type, onClick }) => {
+  const btnType = ["positive", "negative"].includes(type) ? type : "default"; // type이 positive, negative가 아니면 default로 설정
+  return (
+    <button
+      className={["MyButton", `MyButton_${btnType}`].join(" ")}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+MyButton.defaultProps = {
+  type: "default",
+};
+
+export default MyButton;
